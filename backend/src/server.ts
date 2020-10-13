eb1066df2eeb223bf6854a7f0f757548eebc8656
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-10-13 14:56:36 -0300

    init connections database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 
+import './database/connections'
+
 const app = express();
+app.use(express.json());
+app.get('/', (req, res)=>{
+    res.send("Ok")
+    console.log('teste');
+})
 
 
 app.listen(3333);
