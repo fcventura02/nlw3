a0fafdc0a1e80c1f58e8e97c99571217e2c270da
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-10-13 16:23:29 -0300

    created table orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,39 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './model/Orphanage';
 
 import './database/connections'
 
 const app = express();
 app.use(express.json());
-app.get('/', (req, res)=>{
-    res.send("Ok")
-    console.log('teste');
-})
+
+
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longetude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = req.body
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longetude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    })
+
+    await orphanagesRepository.save(orphanage)
+    
+    return res.status(201).json({ok:"true"})
+});
 
 
 app.listen(3333);
