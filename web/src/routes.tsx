1ad5806b6e6720073e10e6b21ad343a91cdfaf82
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-10-14 10:03:42 -0300

    Scope of pages created to create orphanage and orphanage

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanatesMap from './pages/OphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
 function Routes() {
     return (
@@ -10,6 +12,10 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanatesMap} />
+
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+
             </Switch>
         </BrowserRouter>
     )
