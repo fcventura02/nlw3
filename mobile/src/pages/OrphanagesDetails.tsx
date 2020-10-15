c738e37671722061ce983ffa3aa9dcc39d1c2155
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-10-15 12:08:01 -0300

    created orfanagesMap and started routes

---
+++ mobile/src/pages/OrphanagesDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const OrphanagesDetails = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default OrphanagesDetails
+
+const styles = StyleSheet.create({})
